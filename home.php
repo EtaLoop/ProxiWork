31d4b080d204d88b86ee6de48009fafb1bf0f820
pintat-gil <diego.pintat-gil@gmail.com>
2025-01-15 12:00:12 +0100

    css login page

---
+++ home.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" type="text/css" href="./css/index.css">
+</head>
+<body>
+
+  <div className="App">
+
+    <!--    ------------------Header-----------------      -->
+    <header class="app-header">
+      <img class="logo-header child-header" src="./assets/img/logo.png" />
+      <input class="search-container child-header" type="input" placeholder='Rechercher une offre' />
+      <button class="create-annonce child-header">Créer votre propre annonce !</button>
+      <img class="compte-container" src="./assets/img/account.png" />
+    </header>
+    <!--    ------------------END Header-----------------      -->
+
+
+    <div class="body-container">
+      <div id="map-container" >
+          <iframe 
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.835434509687!2d-122.41941508468177!3d37.77492977975937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8085808c0f8a4cb5%3A0x4f0dcb9264e1f6b9!2sSan%20Francisco%2C%20CA%2C%20USA!5e0!3m2!1sen!2sfr!4v1615297974572!5m2!1sen!2sfr" 
+              allowfullscreen="yes" 
+              loading="lazy">
+          </iframe>
+      </div>
+      <div class="post-container">
+        <div class="post"> Annonce </div>
+        <div class="post pair"> Annonce </div>
+        <div class="post"> Annonce </div>
+        <div class="post pair"> Annonce </div>
+        <div class="post"> Annonce </div>
+        <div class="post pair"> Annonce </div>
+        <div class="post"> Annonce </div>
+        <div class="post pair"> Annonce </div>
+        <div class="post"> Annonce </div>
+        <div class="post pair"> Annonce </div>
+        <div class="post"> Annonce </div>
+        <div class="post pair"> Annonce </div>
+        
+
+
+
+        <!--    ------------------FOOTER-----------------      -->
+        <footer class="footer">
+          <div id="footer-socials">
+            <div class="socials inline-inside socials-colored">
+              <a href="#" target="_blank" title="Facebook" class="socials-item">
+                <i class="fab fa-facebook-f facebook"></i>
+              </a>
+              <a href="#" target="_blank" title="Twitter" class="socials-item">
+                <i class="fab fa-twitter twitter"></i>
+      
+              </a>
+              <a href="#" target="_blank" title="Instagram" class="socials-item">
+                <i class="fab fa-instagram instagram"></i>
+              </a>
+              <a href="#" target="_blank" title="YouTube" class="socials-item">
+                <i class="fab fa-youtube youtube"></i>
+              </a>
+              <a href="#" target="_blank" title="Telegram" class="socials-item">
+                <i class="fab fa-telegram telegram"></i>
+              </a>
+            </div>
+          </div>
+        </footer>
+        <!--                     END  FOOTER                          -->
+
+
+      </div>
+    </div>
+  </div>
+  
+</body>
+</html>
+
+
+
+
+
+
+
